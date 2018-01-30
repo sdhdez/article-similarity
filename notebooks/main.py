44ed3599c2446d9cf850e7ca21dfb548af150c14
Simon David HERNANDEZ <simondhp@git.totum.one>
2018-01-30 11:37:42 +0100

    jaccard similarity matrix

--- notebooks/main.py
+++ notebooks/main.py
@@ -4,13 +4,16 @@ import measures.jaccard as mjaccard
 
 if __name__ == "__main__":
     print("Main from command-line.", file=sys.stderr)
-  
-    doc_list = {}
-    for pfilename in rd.get_filenames("./resources/test"):
+    doc_list = []
+    for pfilename in rd.get_filenames("./resources/aminer/v1"):
         for indexdoc, text in rd.get_target_text(pfilename):
             bag_of_words = rd.get_bag_of_words(rd.get_tokens_default(text.lower()))
-            doc_list[indexdoc] = { 'set': bag_of_words, 
-                                    'cardinality': len(bag_of_words)}
-    #print([doc_list[d]['cardinality'] for d in doc_list])
-    mjaccard.distance_matrix(doc_list)
+            doc_list.append({ 'set': bag_of_words, 
+                                'cardinality': len(bag_of_words),
+                                'index': indexdoc
+                                })
+    print("Documents: ", len(doc_list))
+    J = mjaccard.similarity_matrix(doc_list)
+    print("J.shape ", J.shape)
+    print(J[98,99])
 
--- notebooks/measures/jaccard.py
+++ notebooks/measures/jaccard.py
@@ -1,15 +1,25 @@
 import numpy as np
+from scipy.sparse import dok_matrix
 
 def jaccard_index(set_A, set_B):
-    card_intersection_A_B = len(set_A['set'] & set_B['set']) * 1.0
+    card_intersection_A_B = np.float32(len(set_A['set'] & set_B['set']))
     return card_intersection_A_B /(set_A['cardinality'] + set_B['cardinality'] - card_intersection_A_B)
 
-def distance_matrix(doc_list):
+# def jaccard_distance(set_A, set_B):
+#    return 1.0 - jaccard_index(set_A, set_B)
+
+def similarity_matrix(doc_list):
     N = len(doc_list)
-    dmatrix = np.zeros([N, N])
-    for A in doc_list:
-        for B in doc_list:
-            print(jaccard_index(doc_list[A], doc_list[B]))
-
-    
-
+    dmatrix = dok_matrix((N, N), dtype=np.float32)
+    i = 0
+    while i < N:
+        j = i
+        while j < N:
+            jd = jaccard_index(doc_list[i], doc_list[j])
+            if jd != 0.0:
+                dmatrix[i,j] = jd 
+                if i != j:
+                    dmatrix[j,i] = jd 
+            j += 1
+        i += 1
+    return dmatrix
