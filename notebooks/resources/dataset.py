9627fdc079442e79df648d96425f2962573ee731
Simon Hernandez <sdavidhdez@gitlab.totum.one>
2018-02-03 15:18:16 +0000

    Examples to search in indexed data
    Signed-off-by: Simon Hernandez <sdavidhdez@gitlab.totum.one>

--- notebooks/resources/dataset.py
+++ notebooks/resources/dataset.py
@@ -1,11 +1,14 @@
 import sys, os.path
 import string
 import numpy as np
-from whoosh.index import create_in
+from whoosh.index import create_in, open_dir
 from whoosh.fields import Schema, TEXT, ID, KEYWORD, NUMERIC
 from whoosh.analysis import StemmingAnalyzer, RegexTokenizer, LowercaseFilter, StopFilter
-
 from whoosh.writing import BufferedWriter
+from whoosh import qparser
+from whoosh.qparser import QueryParser
+
+from matrices.indexing import INDEX_DATA, INDEX_DATA_SAMPLE, INDEX_JACCARD, INDEX_WORD2VEC
 
 def get_filenames(path_resources):
     file_count = 0
@@ -37,7 +40,7 @@ def get_target_text(pfilename):
 def create_bow_from_files_in(documents_path): 
     data_batch = 50000
 
-    whoosh_index = documents_path + "/index-data"
+    whoosh_index = documents_path + INDEX_DATA
     if not os.path.exists(whoosh_index):
         os.mkdir(whoosh_index)
 
@@ -70,3 +73,42 @@ def create_bow_from_files_in(documents_path):
                 print("N: ", i)
             i += 1
     writer.commit()
+
+def get_cursor(index_data_path):
+    if os.path.exists(index_data_path):
+        ix_data = open_dir(index_data_path)
+        return ix_data
+
+def cur_indexed_docs(data_path):
+    index_data_path = data_path + INDEX_DATA
+    return get_cursor(index_data_path)
+
+def cur_indexed_sample(data_path):
+    index_data_path = data_path + INDEX_DATA_SAMPLE
+    return get_cursor(index_data_path)
+
+def cur_indexed_jaccard(data_path):
+    index_data_path = data_path + INDEX_DATA_JACCARD
+    return get_cursor(index_data_path)
+
+def cur_indexed_word2vec(data_path):
+    index_data_path = data_path + INDEX_DATA_WORD2VEC
+    return get_cursor(index_data_path)
+
+def find_indexdoc(ix_data, query, doc_limit=1):
+    parser = QueryParser("indexdoc", ix_data.schema)
+    q = parser.parse(query)
+    result = searcher.search(q, limit = doc_limit)
+    return result[0]
+
+def find_in_content(ix_data, query, doc_limit=10):
+    parser = QueryParser("content", ix_data.schema)
+    q = parser.parse(query)
+    result = searcher.search(q, limit = doc_limit)
+    return result
+
+def find_in_bag_of_words(ix_data, query, doc_limit=10):
+    parser = QueryParser("bag_of_words", ix_data.schema)
+    q = parser.parse(query)
+    result = searcher.search(q, limit = doc_limit)
+    return result
