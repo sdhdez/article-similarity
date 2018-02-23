78bd88e9d69619bef977f73ea4f662785fa95d36
Simon David HERNANDEZ <simondhp@git.totum.one>
2018-02-23 17:48:45 +0100

    data samples, frequency of sim >0.0
    Signed-off-by: Simon David HERNANDEZ <simondhp@git.totum.one>

--- context-in-articles/resources/dataset.py
+++ context-in-articles/resources/dataset.py
@@ -75,9 +75,7 @@ def create_bow_from_files_in(documents_path):
     writer.commit()
 
 def get_cursor(index_data_path):
-    print(index_data_path)
     if os.path.exists(index_data_path):
-        print(index_data_path)
         ix_data = open_dir(index_data_path)
         return ix_data
 
@@ -85,8 +83,8 @@ def cur_indexed_docs(data_path):
     index_data_path = data_path + INDEX_DATA
     return get_cursor(index_data_path)
 
-def cur_indexed_sample(data_path):
-    index_data_path = data_path + INDEX_DATA_SAMPLE
+def cur_indexed_sample(data_path, nrand=100, doc_limit=10):
+    index_data_path = data_path + INDEX_DATA_SAMPLE + "-%d-%d" % (nrand, doc_limit)
     return get_cursor(index_data_path)
 
 def cur_indexed_jaccard(data_path):
@@ -102,18 +100,23 @@ def find_indexdoc(ix_data, query, doc_limit=1):
     q = parser.parse(query)
     with ix_data.searcher() as searcher:
         result = searcher.search(q, limit = doc_limit)
-    return result[0]
+        for r in result:
+             yield r
 
 def find_in_content(ix_data, query, doc_limit=10):
     parser = QueryParser("content", ix_data.schema)
     q = parser.parse(query)
     with ix_data.searcher() as searcher:
         result = searcher.search(q, limit = doc_limit)
-    return result
+        print("Documents found: ", len(result))
+        for r in result:
+             yield r
 
 def find_in_bag_of_words(ix_data, query, doc_limit=10):
     parser = QueryParser("bag_of_words", ix_data.schema)
     q = parser.parse(query)
     with ix_data.searcher() as searcher:
         result = searcher.search(q, limit = doc_limit)
-    return result
+        print("Documents found: ", len(result))
+        for r in result:
+            yield r
